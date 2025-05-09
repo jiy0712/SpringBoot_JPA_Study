67a7c523caf5e46e2e7a797c6c78a803b49179fa
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-10 06:43:20 +0900

    h2 DB 오류

--- src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.jpa.bookmanager.repository;
 
 import com.jpa.bookmanager.domain.Users;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,11 +18,17 @@ class UserRepositoryTest {
 
     @Test
     void crud(){ //create, read, update, delete
-        userRepository.save(new Users()); //테이블에 저장하겠다는 의미
+        Users user1 = new Users("jack", "jack@gmail.com");
+        Users user2 = new Users("jack2", "jack2@gmail.com");
 
-        userRepository.findAll().forEach(System.out::println);
+        userRepository.saveAll(Lists.newArrayList(user1,user2));
+        //userRepository.save(user1);
 
+        List<Users> users = userRepository.findAll();
+        users.forEach(System.out::println);
 
+        //List<Users> users = userRepository.findAllById(findAll(Sort.by(Sort.Direction.DESC,"name"));
+        //List<Users> users = userRepository.findAllById(Lists.newArrayList(1L,3L,5L));
     }
 
 }
